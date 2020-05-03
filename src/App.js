c3672487ef1bbbdf443dfd68853187ba1183c1e5
Regis Ribeiro <regisgomesr@gmail.com>
2020-05-03 18:42:16 -0300

    Cards - Fetching people's data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React from 'react'
 
+// import Cards from './components/Cards/Cards'
+// import Chart from './components/Chart/Chart'
+// import CountryPicker from './components/CountryPicker/CountryPicker'
+
+import { Cards, Chart, CountryPicker } from './components'
+import { fetchData } from './api'
+import styles from './App.module.css'
+
 class App extends React.Component {
+    state = {
+        data: {},
+    }
+
+    async componentDidMount() {
+        const data = await fetchData()
+
+        this.setState({ data })
+    }
+
     render() {
+        const { data } = this.state
+
         return (
-            <div>
-                <h1>App</h1>
+            <div className={styles.container}>
+                <Cards data={data} />
+                <CountryPicker />
+                <Chart />
             </div>
         )
     }
